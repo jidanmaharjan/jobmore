221d27f18830ffd4fc8fa878f0ad46aee248b4e0
jidanmaharjan <jidanmaharjan1@gmail.com>
2024-12-10 16:50:14 +0545

    home screen changes

--- app/index.jsx
+++ app/index.jsx
@@ -1,42 +1,46 @@
-import { Link } from 'expo-router'
-import { StatusBar } from 'expo-status-bar'
-import { useEffect, useState } from 'react'
-import { ActivityIndicator, StyleSheet, Text, View } from 'react-native'
+import { useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  Image,
+  SafeAreaView,
+  ScrollView,
+  View,
+} from "react-native";
 
-const Home = () => {
-  const [loading, setLoading] = useState(true)
+import { images } from "../constants";
+
+const App = () => {
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     setTimeout(() => {
-      setLoading(false)
-    }, 3000)
-  }, [])
-  if( loading) {
+      setLoading(false);
+    }, 1000);
+  }, []);
+  if (loading) {
     return (
-      <View style={
-        styles.container
-      }>
-      <ActivityIndicator size={'large'} color={'orange'} />
+      <View className="flex-1 items-center justify-center">
+        <ActivityIndicator size={"large"} color={"orange"} />
       </View>
-    )
+    );
   }
   return (
-    <View style={
-      styles.container
-    }>
-      <Text className="text-red-400">MAIN</Text>
-      <StatusBar />
-      <Link href='/home' style={{color: 'blue'}}>GO TO HOME</Link>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+        <View className="items-center justify-center w-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            style={{ width: 130, height: 84 }}
+            resizeMode="contain"
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-export default Home
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-})
+export default App;
