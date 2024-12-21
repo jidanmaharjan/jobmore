eebf655eec1f930b8811076589686aa450ac7d9f
jidanmaharjan <jidanmaharjan1@gmail.com>
2024-12-21 23:40:22 +0545

    sign in form

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,8 +1,11 @@
-import { View, Text } from "react-native";
+import { View, Text, TextInput } from "react-native";
 const SignIn = () => {
   return (
     <View>
       <Text>SignIn</Text>
+      <TextInput placeholder="Email" />
+      <TextInput placeholder="Password" />
+      <CustomButton title="Sign In" />
     </View>
   );
 };
