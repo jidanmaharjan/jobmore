c0ee7a048970bb15f472fdb3e741d50382482888
jidanmaharjan <jidanmaharjan1@gmail.com>
2024-12-22 23:57:45 +0545

    custom formfield

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,13 +1,29 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, SafeAreaView, ScrollView, Image } from "react-native";
 import CustomButton from "../../components/CustomButton";
+
+import { images } from "../../constants";
+
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-      <TextInput placeholder="Email" />
-      <TextInput placeholder="Password" />
-      <CustomButton title="Sign In" />
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="items-center justify-center w-full px-4 min-h-[85vh]">
+          <Image source={images.logo} className="w-[115px] h-[35px]" />
+          <Text className="text-white font-psemibold text-2xl mt-10">Sign In to your account</Text>
+          <FormField
+            title="Email"
+            value="form.email"
+            placeholder="Enter your email"
+            handleTextchange={(text) => setForm((prev) => ({ ...prev, email: text }))}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+          <TextInput placeholder="Email" />
+          <TextInput placeholder="Password" />
+          <CustomButton title="Sign In" />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 export default SignIn;
---
+++ components/CustomFormField.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CustomFormField = () => {
+  return (
+    <View className="space-y-2">
+      <Text className="text-base text-gray-700 font-pmedium">CustomFormField</Text>
+    </View>
+  )
+}
+
+export default CustomFormField
