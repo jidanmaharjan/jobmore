2c6aed5c196c7b5ca413003c44ee2854d5027685
jidanmaharjan <jidanmaharjan1@gmail.com>
2024-12-23 17:48:37 +0545

    sign in UI

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,26 +1,56 @@
-import { View, Text, TextInput, SafeAreaView, ScrollView, Image } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  SafeAreaView,
+  ScrollView,
+  Image,
+} from "react-native";
 import CustomButton from "../../components/CustomButton";
 
 import { images } from "../../constants";
+import CustomFormField from "../../components/CustomFormField";
+import { useState } from "react";
+import { router } from "expo-router";
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className="items-center justify-center w-full px-4 min-h-[85vh]">
-          <Image source={images.logo} className="w-[115px] h-[35px]" />
-          <Text className="text-white font-psemibold text-2xl mt-10">Sign In to your account</Text>
-          <FormField
+        <View className="items-center justify-center w-full px-6 min-h-[85vh]">
+          <Image source={images.logo} className="w-[115px] h-[35px]" style={{ width: 115, height: 35 }} />
+          <Text className="text-white font-psemibold text-2xl mt-10">
+            Sign In to your account
+          </Text>
+          <CustomFormField
             title="Email"
-            value="form.email"
+            value={form.email}
             placeholder="Enter your email"
-            handleTextchange={(text) => setForm((prev) => ({ ...prev, email: text }))}
+            handleTextchange={(e) => setForm((prev) => ({ ...prev, email: e }))}
             otherStyles="mt-7"
             keyboardType="email-address"
           />
-          <TextInput placeholder="Email" />
-          <TextInput placeholder="Password" />
-          <CustomButton title="Sign In" />
+          <CustomFormField
+            title="Password"
+            value={form.password}
+            placeholder="Enter your password"
+            handleTextchange={(e) =>
+              setForm((prev) => ({ ...prev, password: e }))
+            }
+            otherStyles="mt-7"
+            keyboardType="password"
+          />
+          <CustomButton
+            title="Sign In"
+            handlePress={() => router.push("/home")}
+            isLoading={false}
+            containerStyles={"w-full mt-7"}
+            textStyles={"text-white"}
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
--- components/CustomFormField.jsx
+++ components/CustomFormField.jsx
@@ -1,12 +1,50 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  TextInput,
+  Touchable,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import React, { useState } from "react";
 
-const CustomFormField = () => {
+import { icons } from "../constants";
+
+const CustomFormField = ({
+  title,
+  value,
+  placeholder,
+  handleTextchange,
+  otherStyles,
+  ...props
+}) => {
+  const [showPassword, setShowPassword] = useState(false);
   return (
-    <View className="space-y-2">
-      <Text className="text-base text-gray-700 font-pmedium">CustomFormField</Text>
+    <View className={`space-y-2 w-full ${otherStyles}`}>
+      <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
+      <View className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center justify-between flex-row">
+        <TextInput
+          className="flex-1 text-white font-psemibold text-base "
+          value={value}
+          placeholder={placeholder}
+          onChangeText={handleTextchange}
+          placeholderTextColor={"#7b7b8b"}
+          secureTextEntry={title === "Password" && !showPassword}
+          {...props}
+        />
+        {title === "Password" && (
+          <TouchableOpacity onPress={() => setShowPassword((prev) => !prev)}>
+            <Image
+              source={showPassword ? icons.eyeHide : icons.eye}
+              className="w-6 h-6"
+              style={{ width: 24, height: 24 }}
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
+        )}
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default CustomFormField
+export default CustomFormField;
