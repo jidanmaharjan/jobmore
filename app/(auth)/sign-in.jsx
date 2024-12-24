c20f7503767d067aefd68110b3d19b1498413915
jidanmaharjan <jidanmaharjan1@gmail.com>
2024-12-24 14:41:13 +0545

    crash fix and UX changes

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,48 +1,54 @@
-import {
-  View,
-  Text,
-  TextInput,
-  SafeAreaView,
-  ScrollView,
-  Image,
-} from "react-native";
+import { Image, SafeAreaView, ScrollView, Text, View } from "react-native";
 import CustomButton from "../../components/CustomButton";
 
-import { images } from "../../constants";
-import CustomFormField from "../../components/CustomFormField";
-import { useState } from "react";
 import { router } from "expo-router";
+import { useState } from "react";
+import CustomFormField from "../../components/CustomFormField";
+import { images } from "../../constants";
 
 const SignIn = () => {
   const [form, setForm] = useState({
     email: "",
     password: "",
   });
+  const [focused, setFocused] = useState(false);
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className="items-center justify-center w-full px-6 min-h-[85vh]">
-          <Image source={images.logo} className="w-[115px] h-[35px]" style={{ width: 115, height: 35 }} />
+        <View
+          className={`items-center justify-center w-full px-6 min-h-[85vh] transition-all duration-100 ${
+            focused ? "-mt-40" : ""
+          }`}
+        >
+          <Image
+            source={images?.logo}
+            className="w-[115px] h-[35px]"
+            style={{ width: 115, height: 35 }}
+            resizeMode="contain"
+          />
           <Text className="text-white font-psemibold text-2xl mt-10">
             Sign In to your account
           </Text>
           <CustomFormField
             title="Email"
-            value={form.email}
+            value={form?.email}
             placeholder="Enter your email"
             handleTextchange={(e) => setForm((prev) => ({ ...prev, email: e }))}
             otherStyles="mt-7"
             keyboardType="email-address"
+            onFocus={() => setFocused(true)}
+            onBlur={() => setFocused(false)}
           />
           <CustomFormField
             title="Password"
-            value={form.password}
+            value={form?.password}
             placeholder="Enter your password"
             handleTextchange={(e) =>
               setForm((prev) => ({ ...prev, password: e }))
             }
             otherStyles="mt-7"
-            keyboardType="password"
+            onFocus={() => setFocused(true)}
+            onBlur={() => setFocused(false)}
           />
           <CustomButton
             title="Sign In"
--- components/CustomFormField.jsx
+++ components/CustomFormField.jsx
@@ -1,12 +1,11 @@
+import React, { useState } from "react";
 import {
-  View,
+  Image,
   Text,
   TextInput,
-  Touchable,
   TouchableOpacity,
-  Image,
+  View
 } from "react-native";
-import React, { useState } from "react";
 
 import { icons } from "../constants";
 
@@ -21,7 +20,7 @@ const CustomFormField = ({
   const [showPassword, setShowPassword] = useState(false);
   return (
     <View className={`space-y-2 w-full ${otherStyles}`}>
-      <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
+      <Text className="text-base text-gray-100 font-pmedium mb-2">{title}</Text>
       <View className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center justify-between flex-row">
         <TextInput
           className="flex-1 text-white font-psemibold text-base "
@@ -35,7 +34,7 @@ const CustomFormField = ({
         {title === "Password" && (
           <TouchableOpacity onPress={() => setShowPassword((prev) => !prev)}>
             <Image
-              source={showPassword ? icons.eyeHide : icons.eye}
+              source={showPassword ? icons?.eyeHide : icons?.eye}
               className="w-6 h-6"
               style={{ width: 24, height: 24 }}
               resizeMode="contain"
