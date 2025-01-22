8fa830c6c9275ed94f2d87e8362fa6984f5903b7
jidanmaharjan <jidanmaharjan1@gmail.com>
2025-01-22 23:04:25 +0545

    added carousel

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,6 +1,7 @@
 import { FlatList, Image, RefreshControl, SafeAreaView, Text, View } from "react-native";
 import { images } from "../../constants";
 import { useState } from "react";
+import { Carousel, Colors, Spacings } from "react-native-ui-lib";
 const Home = () => {
   const [refreshing, setRefreshing] = useState(false)
 
@@ -10,13 +11,35 @@ const Home = () => {
       setRefreshing(false)
     }, 2000)
   }
+
+  const BACKGROUND_COLORS = [
+    Colors.red50,
+    Colors.yellow20,
+    Colors.purple50,
+    Colors.green50,
+    Colors.cyan50,
+    Colors.purple20,
+    Colors.blue60,
+    Colors.red10,
+    Colors.green20,
+    Colors.purple60
+  ];
+
+  const Page = ({children, style, ...others}) => {
+    return (
+      <View {...others} style={style} className="flex h-40 rounded-lg justify-center items-center overflow-hidden">
+        {children}
+      </View>
+    );
+  };
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <FlatList
         data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
         keyExtractor={(item) => item.id}
         renderItem={(item) => (
-          <Text className="text-3xl font-psemibold text-white">{item.id}</Text>
+          <Text style={{ color: '#fff'}} className="text-3xl font-psemibold text-white">{item.id}</Text>
         )}
         ListHeaderComponent={() => (
           <View className="my-6 px-4 space-y-6">
@@ -42,6 +65,23 @@ const Home = () => {
         )}
         refreshControl={<RefreshControl tintColor={"#FFA001"} refreshing={refreshing} onRefresh={onRefresh} />}
       />
+      <View className="space-y-6 rounded-lg overflow-hidden m-4">
+        <Carousel
+          autoplay
+          // onChangePage={onChangePage}
+          itemSpacings={Spacings.s3}
+          containerMarginHorizontal={Spacings.s2}
+          initialPage={2}
+          pageControlPosition={Carousel.pageControlPositions.UNDER}
+          // allowAccessibleLayout
+        >
+          {[...Array(10)].map((_item, index) => (
+            <Page style={{backgroundColor: BACKGROUND_COLORS[index]}} key={index}>
+              <Text margin-15>CARD {index}</Text>
+            </Page>
+          ))}
+        </Carousel>
+      </View>
       {/* <ScrollView className="p-4 bg-primary h-full ">
       <View className="gap-4">
         <View className="bg-black-100 border-black-200 border-2 p-4 rounded-md flex flex-column gap-4">
